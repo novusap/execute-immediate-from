51089fa915571b1236f5dd726a3ca7d1b7211349
Michael Tacker <michael.tacker@gapac.com>
2024-11-09 08:52:13 -0500

    Separate SET variables FROM the build code

--- apps/sf_deploy_prd.sql
+++ apps/sf_deploy_prd.sql
@@ -11,7 +11,7 @@
 -- SCHEMAS     
 
 --------------------------------------------------------------------------------------------
--- Approach 1 - Include SET variables WITH the build code:
+-- Approach 1 - Include SET variables WITH the build code: 
 -- EXECUTE IMMEDIATE FROM @SNOWFLAKE_GIT_REPO/branches/master/apps/adm_control/snowflake_objects/databases/schemas/tags_schema/tags_build.sql;
 -- Results in error:
 -- "Unsupported feature 'session variables not supported during object dependencies backfill"
